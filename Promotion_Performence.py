9e3d3dfe7f0fc5e7a671db1bf4c29a03127239e9
Raksa Kun <raksa.kun@heineken.com>
2024-09-04 17:10:42 +0700

    embbed_powerBi

--- Promotion_Performence.py
+++ Promotion_Performence.py
@@ -7,8 +7,11 @@ def main():
     
     st.markdown('<h1 style="color: #205527;">Embedded Power BI Dashboard</h1>', unsafe_allow_html=True)
 
-    # Embedding Power BI report in an iframe
-    st.write("### Power BI Dashboard:")
+    # Define the embedded link
+    embedded_link = "https://app.powerbi.com/reportEmbed?reportId=e7695318-b683-4d14-954b-96f6080bb11f&autoAuth=true&ctid=66e853de-ece3-44dd-9d66-ee6bdf4159d4"
+
+    # Display the embedded link using components
+    st.components.v1.iframe(embedded_link, width=800, height=600)
 
 if __name__ == "__main__":
     main()
